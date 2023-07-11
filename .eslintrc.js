66dfd50a0e19fce80ac6e66607f16e4c3a1ba6be
Dexter096 <gr1nch2@yandex.ru>
2023-07-11 22:12:46 +0300

    chore: add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,46 @@ module.exports = {
     commonjs: true,
     es2021: true
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base', 
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    project: './tsconfig.json'
   },
-  plugin: ['prettier'],
+  plugin: ['prettier', '@typescript-eslint'],
   rules: {
     'import/extensions': 'off',
-    'prettier/prettier': 'error'
-  }
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    "@typescript-eslint/no-inferrable-types": "error",
+    "@typescript-eslint/array-type": [
+      "error",
+      {
+        default: 'array',
+      },
+    ],
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      {
+        accessibility: 'explicit',
+        overrides: {
+          accessors: 'explicit',
+          constructors: 'off',
+          methods: 'explicit',
+          parameterProperties: 'explicit',
+          properties: 'explicit',
+          }
+      }
+    ],
+  },
+  "@typescript-eslint/explicit-function-return-type": "error",
+  "@typescript-eslint/no-unnecessary-type-assertion": "error",
+  "@typescript-eslint/no-non-null-assertion": "error"
 }
   
