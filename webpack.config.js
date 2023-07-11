8363a0f2aebf120185084a3f6302ede4bbb6e3fb
Dexter096 <gr1nch2@yandex.ru>
2023-07-11 19:53:05 +0300

    chore: add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
-	entry: '.index.js',
+	entry: './index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist')
-	}
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [new HtmlWebpackPlugin()],
+	module: {
+    rules: [
+			{
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 }
